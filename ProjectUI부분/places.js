7fc118d27cca6701c57e562ea11bfab49b8d0532
kwoncesc <kwoncesc@gmail.com>
2017-05-26 16:36:02 +0900

    :)

---
+++ ProjectUI부분/places.js
@@ -0,0 +1,45 @@
+var Observable = require("FuseJS/Observable");
+
+module.exports = Observable(
+	{
+		id: 0,
+		name: '종설프 Among',
+		subject: 'Capstone Design',
+		color: '#D3D3D3',
+		image: "Assets/calendar_new.png",
+		numbers: [
+			{ title: '', fact: ''},
+			{ title: '', fact: '' }
+		],
+		facts: [
+
+		]
+	},{
+		id: 1,
+		name: '그래픽스 팀프로젝트2',
+		subject: 'Computer Grapgics',
+		color: '#D3D3D3', // 나중에 색깔 번갈아가면서 가능??
+		image: "Assets/calendar_new.png",
+		numbers: [
+			{ title: '', fact: '' },
+			{ title: '', fact: ''}
+		],
+		facts: [
+
+		]
+	},{
+		id: 2,
+		name: '알고리즘 스터디',
+		subject: 'Algorithms',
+		color: '#D3D3D3',
+		image: "Assets/calendar_new.png",
+		numbers: [
+			{ title: '', fact: '' },
+			{ title: '', fact: '' }
+		],
+		facts:  [
+		
+		]
+	}
+);
+ 
---
+++ 채팅부분/README.md
@@ -0,0 +1,13 @@
+socket.io example for Fuse [![Build Status](https://travis-ci.org/bolav/fuse-example-using-socketio.svg?branch=master)](https://travis-ci.org/bolav/fuse-example-using-socketio) ![Fuse Version](http://fuse-version.herokuapp.com/?repo=https://github.com/bolav/fuse-example-using-socketio)
+==========================
+
+socket.io example for Fuse.
+
+I did this:
+
+1. `wget https://cdn.socket.io/socket.io-1.4.5.js`
+2. Added a navigator shim
+
+To get the chat server for socket.io:
+
+3. Download https://github.com/rauchg/chat-example and you are good to go, with your own multiplatform chat!
